aebc3ab88111ba2c6a3d47b2304de305537f7c46
digi-monkey <digimonkey@protonmail.com>
2022-05-31 13:48:20 +0800

    refactor(client): rm testJs

--- packages/client/src/router.tsx
+++ packages/client/src/router.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Intro } from "./components/nft/Intro";
 import { Unseal } from "./components/dashboard/Unseal";
-import { TestJs } from "./components/test/TestJs";
 import { Write } from "./components/dashboard/Write";
 import { User } from "./components/dashboard/User";
 
@@ -18,9 +17,6 @@ export default function MyRouter() {
         </Route>
         <Route path="/unseal">
           <Unseal />
-        </Route>
-        <Route path="/test">
-          <TestJs />
         </Route>
         <Route path="/nft">
           <Intro />
