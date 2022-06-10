ecd3cda94a3cce651603717b2bc14d374ea76104
digi-monkey <digimonkey@protonmail.com>
2022-06-10 20:38:56 +0800

    BREAK CHANGE: adjust spilitter character

---
+++ packages/sdk/src/const.ts
@@ -0,0 +1,1 @@
+export const OFFICIAL_SEAL_HOME = "https://underplay.xyz";
--- packages/sdk/src/regex.ts
+++ packages/sdk/src/regex.ts
@@ -1,18 +1,19 @@
+import { OFFICIAL_SEAL_HOME } from "./const";
 const { version: defaultVersion } = require("../package.json");
 
 export const WRITE_SEAL_SPLITTER = "---seal---"; // writer use this splitter to denote which content he/she wants to seal
 
 export const SEALED_SPLITTER_REGEX =
-  /(\s)+······click \[here(.)+to read encrypted content. Power By \[SealBlog\]\([^·]+·······(\s)+/g;
+  /(\s)+······Click \[here(.)+to read encrypted content. Power By \[SealBlog\]\([^·]+·······(\s)+/g;
 
 export const SEAL_CONTENT_MATCH_REGEX =
-  /(\s)*······click[\s]here(.)+to[\s]read[\s]encrypted[\s]content.[\s]Power[\s]By[\s]SealBlog(.)+(\s)+[\s\S]+Power[\s]By[\s]SealBlog·······/;
+  /(\s)*······Click[\s]here(.)+to[\s]read[\s]encrypted[\s]content.[\s]Power[\s]By[\s]SealBlog(.)+(\s)+[\s\S]+Power[\s]By[\s]SealBlog·······/;
 
 export const SPLITTER_REGEX =
-  /\n\n······click[\s]here[\s]to[\s]read[\s]encrypted[\s]content.[\s]Power[\s]By[\s]SealBlog·······/;
+  /\n\n······Click[\s]here[\s]to[\s]read[\s]encrypted[\s]content.[\s]Power[\s]By[\s]SealBlog·······/;
 
 export const SPLITTER_HTML_REGEX =
-  /<[^>]+>······click(.)+unseal?(.)+postId=0x(.)+to[\s]read[\s]encrypted[\s]content.[\s]Power[\s]By(.)+SealBlog<\/a>·······\<\/[a-zA-Z0-9]+\>/;
+  /<[^>]+>······Click(.)+unseal?(.)+postId=0x(.)+to[\s]read[\s]encrypted[\s]content.[\s]Power[\s]By(.)+SealBlog<\/a>·······\<\/[a-zA-Z0-9]+\>/;
 
 export const UNSEAL_SPLITTER_TEXT = "~~~~~~~~~~~ / ᵒᴥᵒᶅ ~~~~~~~~~~~";
 
@@ -21,5 +22,5 @@ export function getFullMatchSealedSplitter(
   decryptUrl: string,
   version: string = defaultVersion
 ) {
-  return `\n\n······click [here](${decryptUrl}?version=${version}&postId=${postId}) to read encrypted content. Power By [SealBlog]()·······\n\n`;
+  return `\n\n······Click [here](${decryptUrl}?version=${version}&postId=${postId}) to read encrypted content. Power By [SealBlog](${OFFICIAL_SEAL_HOME})·······\n\n`;
 }
