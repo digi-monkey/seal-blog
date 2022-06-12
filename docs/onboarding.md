018310a23ae84b1e6915d8b2a78541acdd011183
digi-monkey <digimonkey@protonmail.com>
2022-06-12 16:27:08 +0800

    chore: post manage ui/home page

---
+++ docs/onboarding.md
@@ -0,0 +1,15 @@
+# User Onboarding Guide
+
+Require:
+
+- Install Metamask extension.
+- Use Brave browser or Chrome. Firefox might have some compatible problem right now.
+- Set up Godwoken Layer2 Account
+
+## Blogger
+
+
+
+## Readers
+
+
--- packages/client/src/components/dashboard/Post.tsx
+++ packages/client/src/components/dashboard/Post.tsx
@@ -9,7 +9,9 @@ import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { API_SERVER_URL } from "../../configs";
 import { Account } from "../metamask/account";
-import { Grid } from "@material-ui/core";
+import { Card, Grid } from "@material-ui/core";
+import ReactMarkdown from "react-markdown";
+import ReactDOMServer from "react-dom/server";
 
 const api = new Api(API_SERVER_URL);
 
@@ -33,6 +35,10 @@ const styles = {
     padding: "5px",
     fontSize: "12px",
     color: "gray",
+  },
+  article: {
+    padding: "10px",
+    margin: "10px",
   },
 };
 
@@ -58,10 +64,12 @@ export function Post() {
   const [keyEnvelop, setKeyEnvelop] = useState<HexStr>();
   const [account, setAccount] = useState<HexStr>();
   const [keyAndIv, setKeyAndIv] = useState<KeyAndIv>();
+  const [rawArticleData, setRawArticleData] = useState<string>();
 
   useEffect(() => {
     getTotalEnvelops();
     getKeyEnvelop();
+    loadRawArticle();
   }, []);
 
   const getTotalEnvelops = async () => {
@@ -99,6 +107,35 @@ export function Post() {
     }
   };
 
+  // todo: don't copy code from unseal page
+  const loadRawArticle = async () => {
+    const data = (await api.getPost(postId)).text;
+    if (data === null) {
+      return alert("load raw article failed...");
+    }
+
+    await setRawArticleData(data);
+  };
+
+  const copySealedPostMarkdown = async () => {
+    /* Copy the markdown text inside the text field */
+    setTimeout(async () => {
+      await navigator.clipboard.writeText(rawArticleData!);
+      alert("copy markdown text!");
+    }, 300);
+  };
+
+  const copySealedPostHtml = async () => {
+    const content = <ReactMarkdown>{rawArticleData!}</ReactMarkdown>;
+    /* Copy the html text inside the text field */
+    setTimeout(async () => {
+      await navigator.clipboard.writeText(
+        ReactDOMServer.renderToStaticMarkup(content)
+      );
+      alert("copy html text!");
+    }, 300);
+  };
+
   return (
     <div style={styles.root}>
       <Account accountCallback={setAccount} />
@@ -110,22 +147,81 @@ export function Post() {
           <Heading align={"center"}>Post Manage</Heading>
           <br />
           <div style={styles.info}>
-            Total Envelops: {envelops?.length} --
-            <a href="" onClick={generateEnvelops}>
-              ReGenerate Envelops
-            </a>
-            <Text>
-              <a href="" onClick={revealKey}>
-                Reveal Encrypted AesKey And IV{" "}
-              </a>
-              (Admin Only)
-            </Text>
-            <Text>
-              AesKey: {keyAndIv?.aesKey} IV: {keyAndIv?.iv}
-            </Text>
+            <Grid container spacing={2}>
+              <Grid item xs={7}>
+                <Text transform="capitalize" size={"base"}>
+                  Total Envelops: {envelops?.length}
+                </Text>
+              </Grid>
+              <Grid item xs={5}>
+                <Text transform="capitalize" size={"base"}>
+                  <a href="" onClick={generateEnvelops}>
+                    regenerate envelops
+                  </a>
+                </Text>
+              </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Text
+                  transform="capitalize"
+                  color={"textSecondary"}
+                  size="small"
+                >
+                  an envelop is a encrypted aeskey-iv string specific for one
+                  user to unlock the post. If someone subscribe your blog but
+                  still fail to unlock the post, it might because of the missing
+                  envelop. in that case, you can use the button above to
+                  regenerate.
+                </Text>
+              </Grid>
+            </Grid>
+            <hr />
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Text transform="capitalize" size={"base"}>
+                  <a href="" onClick={revealKey}>
+                    Reveal Encrypted AesKey And IV{" "}
+                  </a>
+                  (Admin Only)
+                </Text>
+              </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                {keyAndIv && (
+                  <Text transform="capitalize" size={"base"}>
+                    AesKey: {keyAndIv?.aesKey}, IV: {keyAndIv?.iv}
+                  </Text>
+                )}
+              </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Text
+                  transform="capitalize"
+                  color={"textSecondary"}
+                  size="small"
+                >
+                  AesKey And IV are the real key to unlock your post.
+                </Text>
+              </Grid>
+            </Grid>
           </div>
         </Grid>
       </Grid>
+      <br />
+      <Heading align={"center"}>Post Content</Heading>
+      <br />
+      <Grid style={{ padding: "10px", textAlign: "center" }}>
+        <Text>
+          <button onClick={copySealedPostMarkdown}>Copy Markdown</button>{" "}
+          <button onClick={copySealedPostHtml}>Copy Html</button>
+        </Text>
+      </Grid>
+      <Card style={styles.article}>
+        <ReactMarkdown>{rawArticleData!}</ReactMarkdown>
+      </Card>
     </div>
   );
 }
